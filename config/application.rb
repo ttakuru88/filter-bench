1dabfab3d2bbbd1c13f360555f4ed962266b756b
Takuru <takuru@hai3.net>
2016-07-29 13:51:39 +0900

    不要な機能を削除した

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+%w(
+  action_controller/railtie
+  action_view/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
